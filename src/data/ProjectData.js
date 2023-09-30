6d925bc3f531cc36932aa559b9097afb07c944ad
amandeepsingh360 <amandeepsingh_ee21b6_50@dtu.ac.in>
2023-09-30 23:26:30 +0530

    Initial Commit

---
+++ src/data/ProjectData.js
@@ -0,0 +1,74 @@
+// --------------------------------------- Project List
+export const ProjectList = [
+  {
+    img: "/renr-responsive.png",
+    title: "EVERYTHING FOR CP",
+    description:
+      "A Full Stack website designed exclusively for enthusiasts to unleash their programming passion.",
+    tech_stack: ["Tailwind", "ReactJs", "NodeJs", "MongoDB", "ChartJs"],
+    github_url: "",
+    demo_url: "https://renr-bcit.web.app/",
+  },
+  {
+    img: "/pokedex-responsive.png",
+    title: "QUICK TALKS",
+    description:
+      "A Dynamic Full Stack website aimed at facilitating quick communication.",
+    tech_stack: ["Tailwind", "ReactJs", "NodeJs", "Socket.io", "Peer Js"],
+    github_url: "https://github.com/salesp07/Pokedex-ReactApp",
+    demo_url: "https://pokedex-salesp07.netlify.app/",
+  },
+];
+
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg",
+    name: "HTML",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg",
+    name: "CSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    name: "JavaScript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+    name: "ReactJS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+    name: "Typescript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg",
+    name: "Git",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg",
+    name: "NodeJS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original.svg",
+    name: "MongoDB",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+    name: "Python",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg",
+    name: "C",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg",
+    name: "Bootstrap",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vscode/vscode-original.svg",
+    name: "VS Code",
+  },
+];
